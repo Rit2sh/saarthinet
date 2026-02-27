937a74e3c862237bd98cb5a92ebff8d7b4166d8b
Ritesh Chauhan <95334507+Rit2sh@users.noreply.github.com>
2026-02-27 21:28:12 +0530

    Delete backend directory

--- backend/auth.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (req, res, next) {
-  const token = req.headers.authorization;
-
-  if (!token) {
-    return res.status(401).json({ error: "Unauthorized request" });
-  }
-
-  next();
-};
--- backend/ratelimit.js
+++
@@ -1,7 +0,0 @@
-const rateLimit = require("express-rate-limit");
-
-module.exports = rateLimit({
-  windowMs: 60 * 1000,
-  max: 20,
-  message: "Too many requests. Try again later."
-});
